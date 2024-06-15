f14ffd12f9e7215f00ad89ab442d9a78f12e9144
Guillermo Andres Rojas Acosta <guillerojas2008@hotmail.com>
2024-06-14 22:20:44 -0500

    Agrega comentarios al API

--- src/app/domains/shared/services/desarrollador.service.ts
+++ src/app/domains/shared/services/desarrollador.service.ts
@@ -1,32 +1,36 @@
-import { Injectable, Signal, inject, signal } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { desarrollador } from './../../../models/desarrollador.model';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DesarrolladorService {
-  desarrolladores = signal<desarrollador[]>([]);
-  private http = inject(HttpClient);
-  
-  constructor() { }
 
+
+export class DesarrolladorService {
+  private http = inject(HttpClient);
+
+  //Método traer registros
   getDesarrollador(){
     return this.http.get<desarrollador[]>('/devs');
   }
 
+  //Método insertar registro
   insertDesarrollador(nickname: string, nombres: string, apellidos: string,){
     return this.http.post<desarrollador[]>('/devs', { nickname, nombres, apellidos });
   }
 
+  //Método actualización registro
   updateDesarrollador(nombres: string, apellidos: string, id: string){
     return this.http.put<desarrollador[]>('/devs/'+id, { nombres, apellidos });
   }
 
+  //Método traer registro específico
   getSelecDesarrollador(id: string){
     return this.http.get<desarrollador[]>('/devs/'+id);
   }
 
+  //Método eliminar registro
   deleteDesarrollador(id: number){
     return this.http.delete<desarrollador[]>('/devs/'+id);
   }
